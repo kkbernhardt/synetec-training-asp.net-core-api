3b130d7c00b0c0c08c7063400dff03593fb4acc8
unknown <kbernhardt@synetec.co.uk>
2019-01-07 15:40:51 +0000

    (feat) CityDto, CitiesDataStore classes

---
+++ CityInfo.API/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,33 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never finished."
+                }
+            };
+        }
+
+    }
+}
