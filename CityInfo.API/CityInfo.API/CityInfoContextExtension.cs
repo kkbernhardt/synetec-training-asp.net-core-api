cf945138ab00bf9a4cfcbf25bb429be4d71d25a9
unknown <kbernhardt@synetec.co.uk>
2019-01-08 12:31:00 +0000

    (feat)add CityInfoContextExtension static class

---
+++ CityInfo.API/CityInfo.API/CityInfoContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public static class CityInfoContextExtension
+    {
+        public static void EnsureSeedDataForContext(this CityInfoContext context)
+        {
+
+        }
+    }
+}
