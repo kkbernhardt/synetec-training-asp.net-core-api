6497812651c8ec13b23cd841e47c5c901277b62b
unknown <kbernhardt@synetec.co.uk>
2019-01-08 09:48:31 +0000

    (feat) add CloudMailService class

---
+++ CityInfo.API/CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private string mailTo = "admin@company.com";
+        private string mailFrom = "noreply@mycompany.com";
+        public void Send(string subject, string message)
+        {
+            Debug.WriteLine($"Mail from {mailFrom} to {mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Subject: {subject}");
+            Debug.WriteLine($"Message: {message}");
+        }
+    }
+}
