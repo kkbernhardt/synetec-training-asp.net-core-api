91a7ae5482156c2e2cf0d4335163ac538dcac04c
unknown <kbernhardt@synetec.co.uk>
2019-01-08 14:09:13 +0000

    (feat)add repositories, Get operation in it

---
+++ CityInfo.API/CityInfo.API/Repositories/CityInfoRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Repositories
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private CityInfoContext context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            this.context = context;
+        }
+
+        public City Get(int cityId, bool includePointOfInterest)
+        {
+            if (includePointOfInterest)
+            {
+                return context.Cities.Include(c => c.PointOfInterest)
+                    .Where(c => c.Id == cityId).FirstOrDefault();
+            }
+            return context.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            return context.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointOfInterestForCity(int cityId)
+        {
+            return context.PointOfInterests.Where(p => p.CityId == cityId).ToList();
+        }
+
+        public PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId)
+        {
+            return context.PointOfInterests
+                .Where(p => p.Id == cityId && p.Id == pointOfInterestId).FirstOrDefault();
+        }
+    }
+}
---
+++ CityInfo.API/CityInfo.API/Repositories/ICityInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Repositories
+{
+    interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City Get(int cityId, bool includePointOfInterest);
+        IEnumerable<PointOfInterest> GetPointOfInterestForCity(int cityId);
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+    }
+}
