d289f7d291bd420e36d1bdfd347b9382bde3b295
unknown <kbernhardt@synetec.co.uk>
2019-01-08 11:34:26 +0000

    (feat)add dummy controller to create database

---
+++ CityInfo.API/CityInfo.API/Controllers/DummyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class DummyController : Controller
+    {
+        private CityInfoContext ctx;
+
+        public DummyController(CityInfoContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        [HttpGet("api/testdatabase")]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
