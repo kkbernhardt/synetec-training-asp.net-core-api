77bc94f1af511d5640c3cd21cb5f0a4468178219
unknown <kbernhardt@synetec.co.uk>
2019-01-08 12:41:28 +0000

    (feat)add data and save changes

--- CityInfo.API/CityInfo.API/CityInfoContextExtension.cs
+++ CityInfo.API/CityInfo.API/CityInfoContextExtension.cs
@@ -10,7 +10,89 @@ namespace CityInfo.API
     {
         public static void EnsureSeedDataForContext(this CityInfoContext context)
         {
+            if (context.Cities.Any())
+            {
+                return;
+            }
 
+            var cities = new List<City>()
+            {
+                new City()
+                {
+                    Name = "New York City",
+                    Description = "The one with that big park.",
+                    PointOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
+                },
+                new City()
+                {
+                    Name = "London",
+                    Description = "The one with smart travel.",
+                    PointOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Hyde park",
+                            Description = "The most visited urban park in England."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Shard",
+                            Description = "A skyscraper located in central London."
+                        }
+                    }
+                },
+                new City()
+                {
+                    Name = "London",
+                    Description = "The one with smart travel.",
+                    PointOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Hyde park",
+                            Description = "The most visited urban park in England."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Shard",
+                            Description = "A skyscraper located in central London."
+                        }
+                    }
+                },
+                new City()
+                {
+                    Name = "Paris",
+                    Description = "The one with the big tower.",
+                    PointOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Eiffel tower",
+                            Description = "The most visited by couples."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "The Louvre",
+                            Description = "The world largest museum."
+                        }
+                    }
+                }
+            };
+
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
         }
     }
 }
