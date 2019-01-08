ffed7672f103fff3db004cc5ba39da307000bcff
unknown <kbernhardt@synetec.co.uk>
2019-01-08 11:17:37 +0000

    (feat)add connection string in Startup

---
+++ CityInfo.API/CityInfo.API/Entities/CityInfoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+
+        /*One way of makind connected to the database:
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connectionstring");
+            base.OnConfiguring(optionsBuilder);
+        }*/
+
+    }
+}
--- CityInfo.API/CityInfo.API/Startup.cs
+++ CityInfo.API/CityInfo.API/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Entities;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -43,9 +45,12 @@ namespace CityInfo.API
             //        castedResolver.NamingStrategy = null;
             //    }
             //});
-            
+           
             services.AddTransient<IMailService, LocalMailService>();
             services.AddTransient<IMailService, CloudMailService>();
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CityInfoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<CityInfoContext>(options => options.UseSqlServer(connectionString));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
